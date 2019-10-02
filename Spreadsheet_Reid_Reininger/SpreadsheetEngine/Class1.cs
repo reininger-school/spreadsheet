3721ea65b29e3b3c721c2921fd6714153e199c7b
Reid Reininger <charles.reininger@wsu.edu>
2019-10-02 16:00:55 -0700

    Install stylecop SpreadsheetEngine project

--- Spreadsheet_Reid_Reininger/SpreadsheetEngine/Class1.cs
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/Class1.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿/*
+Author: Reid Reininger
+Student ID: 11512839
+*/
 namespace SpreadsheetEngine
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Does nothing.
+    /// </summary>
     public class Class1
     {
     }
