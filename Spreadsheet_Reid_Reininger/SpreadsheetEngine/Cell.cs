097547237ce36e37806774c79fa287f385d312b7
Reid Reininger <charles.reininger@wsu.edu>
2019-10-02 19:42:22 -0700

    Add Row/ColumnIndex, Text properties to Cell

--- Spreadsheet_Reid_Reininger/SpreadsheetEngine/Cell.cs
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/Cell.cs
@@ -11,9 +11,37 @@ namespace Cpts321
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Does nothing.
+    /// Represents a cell in the grid.
     /// </summary>
-    public class Cell
+    public abstract class Cell
     {
+        private readonly int rowIndex;
+        private readonly int columnIndex;
+        private string text;
+
+        /// <summary>
+        /// Gets row index cell belongs to.
+        /// </summary>
+        public int RowIndex
+        {
+            get => this.rowIndex;
+        }
+
+        /// <summary>
+        /// Gets column index cell belongs to.
+        /// </summary>
+        public int ColumnIndex
+        {
+            get => this.columnIndex;
+        }
+
+        /// <summary>
+        /// Gets or sets text cell displays.
+        /// </summary>
+        protected string Text
+        {
+            get => this.text;
+            set => this.text = value;
+        }
     }
 }
