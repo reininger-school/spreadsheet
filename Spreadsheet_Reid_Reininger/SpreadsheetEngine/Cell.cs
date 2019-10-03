140013f57bcb009832e59d750b21e56b3c674a74
Reid Reininger <charles.reininger@wsu.edu>
2019-10-02 20:57:16 -0700

    Implement INotifyPropertyChanged in Cell

--- Spreadsheet_Reid_Reininger/SpreadsheetEngine/Cell.cs
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/Cell.cs
@@ -6,6 +6,7 @@ namespace Cpts321
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -13,35 +14,51 @@ namespace Cpts321
     /// <summary>
     /// Represents a cell in the grid.
     /// </summary>
-    public abstract class Cell
+    public abstract class Cell : INotifyPropertyChanged
     {
-        private readonly int rowIndex;
-        private readonly int columnIndex;
-        private string text;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// </summary>
+        /// <param name="rowIndex">Cell's row index.</param>
+        /// <param name="columnIndex">Cell's column index.</param>
+        public Cell(int rowIndex, int columnIndex)
+        {
+            this.RowIndex = rowIndex;
+            this.ColumnIndex = columnIndex;
+        }
+
+        /// <summary>
+        /// Fires everytime property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
         /// Gets row index cell belongs to.
         /// </summary>
-        public int RowIndex
-        {
-            get => this.rowIndex;
-        }
+        public int RowIndex { get; }
 
         /// <summary>
         /// Gets column index cell belongs to.
         /// </summary>
-        public int ColumnIndex
-        {
-            get => this.columnIndex;
-        }
+        public int ColumnIndex { get; }
 
         /// <summary>
         /// Gets or sets text cell displays.
         /// </summary>
         protected string Text
         {
-            get => this.text;
-            set => this.text = value;
+            get => this.Text;
+            set
+            {
+                // do nothing if same text
+                if (value == this.Text)
+                {
+                    return;
+                }
+
+                this.Text = value;
+                this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Text"));
+            }
         }
     }
 }
