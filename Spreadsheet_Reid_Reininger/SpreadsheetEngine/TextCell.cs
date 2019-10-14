9011271dfc34ea5351e92d88ddc66d0019c8e0e1
Reid Reininger <charles.reininger@wsu.edu>
2019-10-14 12:11:20 -0700

    Add TextClass for cells with Text value

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/TextCell.cs
@@ -0,0 +1,25 @@
+﻿namespace SpreadsheetEngine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Cpts321;
+
+    /// <summary>
+    /// Cell with text value.
+    /// </summary>
+    internal class TextCell : Cell
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextCell"/> class.
+        /// </summary>
+        /// <param name="rowIndex">Cell's row index.</param>
+        /// <param name="columnIndex">Cell's column index.</param>
+        public TextCell(int rowIndex, int columnIndex)
+            : base(rowIndex, columnIndex)
+        {
+        }
+    }
+}
