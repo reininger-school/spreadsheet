9142ff393d532d43f474df5975b79384555ad905
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 14:48:29 -0700

    Add ExpressionTree and HW5 method prototypes

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/ExpressionTree.cs
@@ -0,0 +1,40 @@
+﻿// Reid Reininger
+// 11512839
+
+namespace Cpts321
+{
+    using System;
+
+    /// <summary>
+    /// Arithmetic expression parser and evaluator.
+    /// </summary>
+    public class ExpressionTree
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionTree"/> class.
+        /// </summary>
+        /// <param name="expression">Expression to construct tree.</param>
+        public ExpressionTree(string expression)
+        {
+        }
+
+        /// <summary>
+        /// Sets the specified variable within the ExpressionTree variables dictionary.
+        /// </summary>
+        /// <param name="variableName">Variable's name.</param>
+        /// <param name="variableValue">Variable's value.</param>
+        public void SetVariable(string variableName, double variableValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Evaluates expression to a double value.
+        /// </summary>
+        /// <returns>Double evaluated value of tree.</returns>
+        public double Evaluate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
