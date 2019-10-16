25ebcf6721452977b45813dede7682e2a6291810
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 15:38:22 -0700

    Add PrintMenu() to print CLI ExpTree menu

--- Spreadsheet_Reid_Reininger/ExpressionTreeApp/Program.cs
+++ Spreadsheet_Reid_Reininger/ExpressionTreeApp/Program.cs
@@ -4,6 +4,7 @@ namespace ExpressionTreeApp
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -18,6 +19,22 @@ namespace ExpressionTreeApp
         /// </summary>
         public static void Main()
         {
+            PrintMenu(Console.Out, null);
+        }
+
+        /// <summary>
+        /// Print expression tree menu.
+        /// </summary>
+        /// <param name="writer">Stream to write to.</param>
+        /// <param name="expression">Current expression.</param>
+        public static void PrintMenu(TextWriter writer, string expression)
+        {
+            writer.WriteLine($"Menu (current expression=\"{expression}\")");
+            writer.WriteLine("  1 = Enter a new expression");
+            writer.WriteLine("  2 = Set a variable value");
+            writer.WriteLine("  3 = Evaluate tree");
+            writer.WriteLine("  4 = Quit");
+            writer.Flush();
         }
     }
 }
