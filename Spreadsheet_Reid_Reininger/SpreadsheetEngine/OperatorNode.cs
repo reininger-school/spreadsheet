4d23d8e1c7006c782916fc539f39c0c43a2d6a7f
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 16:38:24 -0700

    Add abstract class for operator nodes

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/OperatorNode.cs
@@ -0,0 +1,30 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Cpts321;
+
+    /// <summary>
+    /// Abstract class for operator nodes.
+    /// </summary>
+    internal abstract class OperatorNode : INode
+    {
+        private string op;
+
+        /// <summary>
+        /// Gets string representing operator in expression.
+        /// </summary>
+        public string Op { get; }
+
+        /// <summary>
+        /// Evaluates value of node.
+        /// </summary>
+        /// <returns>Value of node as double.</returns>
+        public abstract double Evaluate();
+    }
+}
