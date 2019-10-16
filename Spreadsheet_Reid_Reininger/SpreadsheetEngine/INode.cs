96d13495bfeff94267b552db46e0a1ba0ea74ca0
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 15:50:23 -0700

    Create INode and add Evaluate interface

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/INode.cs
@@ -0,0 +1,16 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    /// <summary>
+    /// Node interface for expression tree.
+    /// </summary>
+    public interface INode
+    {
+        /// <summary>
+        /// Evaluates value of node.
+        /// </summary>
+        /// <returns>Value of node as double.</returns>
+        double Evaluate();
+    }
+}
