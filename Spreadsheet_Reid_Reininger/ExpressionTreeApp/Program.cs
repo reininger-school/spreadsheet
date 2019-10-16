31ad11db95853301440e8dcbdc8950f5d388893d
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 15:22:23 -0700

    Add Expression Tree console app project

---
+++ Spreadsheet_Reid_Reininger/ExpressionTreeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// Reid Reininger
+// 11512839
+namespace ExpressionTreeApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Console application for ExpressionTree app.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Console app start point.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
