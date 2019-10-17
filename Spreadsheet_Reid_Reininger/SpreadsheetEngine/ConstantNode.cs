0df3e8d4bd8ea921422d8235ad052b4bec24e578
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 21:57:21 -0700

    Add ConstantNode

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/ConstantNode.cs
@@ -0,0 +1,47 @@
+﻿// Reid Reininger
+// 11512839
+
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Cpts321;
+
+    /// <summary>
+    /// Node with constant value.
+    /// </summary>
+    internal class ConstantNode : INode
+    {
+        private double value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstantNode"/> class.
+        /// </summary>
+        /// <param name="value">Value of node.</param>
+        internal ConstantNode(double value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets or sets value of node.
+        /// </summary>
+        private double Value
+        {
+            get => this.value;
+            set => this.value = value;
+        }
+
+        /// <summary>
+        /// Gets value of node.
+        /// </summary>
+        /// <returns>Double value of node.</returns>
+        public double Evaluate()
+        {
+            return this.Value;
+        }
+    }
+}
