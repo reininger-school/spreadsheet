fb03aaecbe85ad324381f739ac3a8da051f8b7f5
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 22:16:52 -0700

    Add SubtractionNode

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/SubtractionNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Cpts321
+{
+    /// <summary>
+    /// Takes difference of operands.
+    /// </summary>
+    internal class SubtractionNode : BinaryOperatorNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubtractionNode"/> class.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        internal SubtractionNode(INode left = null, INode right = null)
+            : base("-", (x, y) => x - y, left, right)
+        {
+        }
+    }
+}
