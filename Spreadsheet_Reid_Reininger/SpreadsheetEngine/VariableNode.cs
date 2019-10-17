31282454c6c3e3be83499150bbe0723e2ca18443
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 22:55:01 -0700

    Add VariableNode

--- Spreadsheet_Reid_Reininger/SpreadsheetEngine/VariableNode.cs
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/VariableNode.cs
@@ -11,11 +11,27 @@ namespace Cpts321
     {
         private double value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VariableNode"/> class.
+        /// </summary>
+        /// <param name="value">Reference to node value.</param>
         internal VariableNode(double value)
         {
             this.value = value;
         }
 
+        /// <summary>
+        /// Gets value of node.
+        /// </summary>
+        private double Value
+        {
+            get => this.value;
+        }
+
+        /// <summary>
+        /// Gets evaluated value of Node.
+        /// </summary>
+        /// <returns>Value of node as double.</returns>
         internal override double Evaluate()
         {
             throw new System.NotImplementedException();
