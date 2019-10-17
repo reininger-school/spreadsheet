fe644c7561411459def2bd35d1909d9b9bf43b18
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 21:58:23 -0700

    Add Test suite for BinaryOperatorNode

---
+++ Spreadsheet_Reid_Reininger/CellTests/TestBinaryOperatorNode.cs
@@ -0,0 +1,29 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Cpts321;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// BinaryOperatorNode test suite.
+    /// </summary>
+    [TestFixture]
+    public class TestBinaryOperatorNode
+    {
+        /// <summary>
+        /// Test Binary node for correct value.
+        /// </summary>
+        [Test]
+        public void TestEvaluate()
+        {
+            var node = new MockBinaryOperatorNode("*", (x, y) => x * y, new ConstantNode(3), new ConstantNode(4));
+            Assert.AreEqual(node.Evaluate(), 12);
+        }
+    }
+}
