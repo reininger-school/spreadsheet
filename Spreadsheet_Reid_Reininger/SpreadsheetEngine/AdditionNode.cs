5bbdaf301438897b461b9ae42dfc116ac43ac50c
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 22:11:50 -0700

    Add AdditionNode

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/AdditionNode.cs
@@ -0,0 +1,20 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    /// <summary>
+    /// Sums operands.
+    /// </summary>
+    internal class AdditionNode : BinaryOperatorNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdditionNode"/> class.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right Operand.</param>
+        internal AdditionNode(INode left = null, INode right = null)
+            : base("+", (x, y) => x + y, left, right)
+        {
+        }
+    }
+}
