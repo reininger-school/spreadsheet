7b1372ecff1cbf051ae8c56a99199d4f93851b0f
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 21:57:45 -0700

    Add concrete MockBinaryOperatorNode for testing

---
+++ Spreadsheet_Reid_Reininger/CellTests/MockBinaryOperatorNode.cs
@@ -0,0 +1,30 @@
+﻿// Reid Reininger
+// 11512839
+
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Cpts321;
+
+    /// <summary>
+    /// Mock concrete class to test BinaryOperatorNode.
+    /// </summary>
+    internal class MockBinaryOperatorNode : BinaryOperatorNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockBinaryOperatorNode"/> class.
+        /// </summary>
+        /// <param name="op">String representing operator in expression.</param>
+        /// <param name="function">Binary function.</param>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        internal MockBinaryOperatorNode(string op, BinaryOp function, INode left, INode right)
+            : base(op, function, left, right)
+        {
+        }
+    }
+}
