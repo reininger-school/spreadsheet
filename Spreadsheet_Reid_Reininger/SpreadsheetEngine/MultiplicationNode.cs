91fafec19de2fcd6620b151260390ebbea249e58
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 22:19:27 -0700

    Add Multiplication Node

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/MultiplicationNode.cs
@@ -0,0 +1,20 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    /// <summary>
+    /// Multiplies operands.
+    /// </summary>
+    internal class MultiplicationNode : BinaryOperatorNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiplicationNode"/> class.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        internal MultiplicationNode(INode left = null, INode right = null)
+            : base("*", (x, y) => x * y, left, right)
+        {
+        }
+    }
+}
