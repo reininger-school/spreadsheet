bbe82484e323a519ab4be445dde9c4b4337865e5
Reid Reininger <charles.reininger@wsu.edu>
2019-10-16 22:22:12 -0700

    Add DivisionNode

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/DivisionNode.cs
@@ -0,0 +1,20 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    /// <summary>
+    /// Divides operands.
+    /// </summary>
+    internal class DivisionNode : BinaryOperatorNode
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DivisionNode"/> class.
+        /// </summary>
+        /// <param name="left">Left operand.</param>
+        /// <param name="right">Right operand.</param>
+        internal DivisionNode(INode left = null, INode right = null)
+            : base("/", (x, y) => x / y, left, right)
+        {
+        }
+    }
+}
