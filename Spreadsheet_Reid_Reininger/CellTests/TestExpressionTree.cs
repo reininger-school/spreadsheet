4c19243204b4e6572d0c6801ed9d887be97de353
Reid Reininger <charles.reininger@wsu.edu>
2019-10-18 12:16:11 -0700

    Fix style warning

--- Spreadsheet_Reid_Reininger/CellTests/TestExpressionTree.cs
+++ Spreadsheet_Reid_Reininger/CellTests/TestExpressionTree.cs
@@ -17,24 +17,6 @@ namespace Cpts321
     public class TestExpressionTree
     {
         private ExpressionTree tree = new ExpressionTree(null);
-        private Node root1;
-
-        /// <summary>
-        /// Create expected result trees.
-        /// </summary>
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            this.root1 = new AdditionNode(new AdditionNode(new ConstantNode(1), new ConstantNode(1)), new ConstantNode(1));
-        }
-
-        /// <summary>
-        /// Test tree builds correctly.
-        /// </summary>
-        /// <param name="expression">Expression to build.</param>
-        public void TestBuildTree(string expression)
-        {
-        }
 
         /// <summary>
         /// Test valid infix to postfix input and result.
@@ -45,7 +27,7 @@ namespace Cpts321
         [TestCase("1+1", new string[] { "+", "1", "1" })]
         [TestCase("1+1+1", new string[] { "+", "1", "+", "1", "1" })]
         [TestCase("11+1", new string[] { "+", "1", "11" })]
-        [TestCase("11+alpha+1+2+beta", new string[] { "+", "beta", "+", "2", "+", "1", "+", "alpha", "11"})]
+        [TestCase("11+alpha+1+2+beta", new string[] { "+", "beta", "+", "2", "+", "1", "+", "alpha", "11" })]
         public void TestInfixToPostfix(string infix, string[] expected)
         {
             this.tree.Expression = infix;
@@ -96,9 +78,11 @@ namespace Cpts321
             Assert.AreEqual(1, node.Evaluate());
         }
 
+        /// <summary>
+        /// Test tree is built correctly.
+        /// </summary>
         public void TestBuildTree()
         {
-
         }
     }
 }
