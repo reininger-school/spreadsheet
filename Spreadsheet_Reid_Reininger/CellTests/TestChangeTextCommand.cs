63e3d5f1418a3bf6952aab1dc7fce4e1a537c597
Reid Reininger <charles.reininger@wsu.edu>
2019-11-05 22:53:31 -0800

    Test Execute on valid input

---
+++ Spreadsheet_Reid_Reininger/CellTests/TestChangeTextCommand.cs
@@ -0,0 +1,35 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// ChangeTextCommand test suite.
+    /// </summary>
+    [TestFixture]
+    public class TestChangeTextCommand
+    {
+        private Cell cell = new MockCell(0, 0);
+
+        /// <summary>
+        /// Test execute with valid parameters.
+        /// </summary>
+        [Test]
+        public void TestValidExecute()
+        {
+            const string testString = "test string";
+            var textInfo = Utility.GetProperty<Cell>("Text");
+            var command = new ChangeTextCommand(this.cell, testString);
+            command.Execute();
+
+            Assert.AreEqual(testString, textInfo.GetValue(this.cell, null));
+        }
+    }
+}
