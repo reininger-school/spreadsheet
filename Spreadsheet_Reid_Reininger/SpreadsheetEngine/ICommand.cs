bcbd4d0cf97d6f7dccee971655db161383bfc30e
Reid Reininger <charles.reininger@wsu.edu>
2019-11-05 22:13:11 -0800

    Add Command interface

---
+++ Spreadsheet_Reid_Reininger/SpreadsheetEngine/ICommand.cs
@@ -0,0 +1,32 @@
+﻿// Reid Reininger
+// 11512389
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Command pattern interface.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Execute command.
+        /// </summary>
+        void Execute();
+
+        /// <summary>
+        /// Perform exact opposite of execute.
+        /// </summary>
+        void Undo();
+
+        /// <summary>
+        /// Get description of what execute does.
+        /// </summary>
+        /// <returns>String description.</returns>
+        string Description();
+    }
+}
