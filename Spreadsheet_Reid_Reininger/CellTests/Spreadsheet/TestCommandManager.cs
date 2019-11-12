b78a589235adabb9938e8f95c8dcb1a67535405b
Reid Reininger <charles.reininger@wsu.edu>
2019-11-11 16:43:11 -0800

    Add TestSuite file for CommandManager

---
+++ Spreadsheet_Reid_Reininger/CellTests/Spreadsheet/TestCommandManager.cs
@@ -0,0 +1,29 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test suite for CommnadManager.
+    /// </summary>
+    [TestFixture]
+    public class TestCommandManager
+    {
+        private CommandManager manager;
+
+        /// <summary>
+        /// Create fresh instanct of CommandManager for each test.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.manager = new CommandManager();
+        }
+    }
+}
