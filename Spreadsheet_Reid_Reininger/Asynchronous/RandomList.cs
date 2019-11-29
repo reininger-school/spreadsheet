1ca65980f6f2642597e94add6f0963fef9d78f1c
Reid Reininger <charles.reininger@wsu.edu>
2019-11-29 11:42:02 -0800

    Test RandomList for in/valid input, check list size

---
+++ Spreadsheet_Reid_Reininger/Asynchronous/RandomList.cs
@@ -0,0 +1,29 @@
+﻿// Reid Reininger
+// 11512839
+namespace Cpts321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Adds constructor to initialize with random values.
+    /// </summary>
+    public static class RandomList
+    {
+        private static Random generator = new Random();
+
+        /// <summary>
+        /// Return list of ranrdom ints with size elements.
+        /// </summary>
+        /// <param name="size">Elements in list.</param>
+        /// <returns>List of random elements.</returns>
+        public static List<int> Random(int size = 1_000_000)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Spreadsheet_Reid_Reininger/TestAsynchronous/TestRandomList.cs
@@ -0,0 +1,63 @@
+﻿// Reid Reininger
+// 11512839
+namespace TestAsynchronous
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using Cpts321;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Test suite for RandomList.
+    /// </summary>
+    [TestFixture]
+    public class TestRandomList
+    {
+        private List<int> list;
+
+        /// <summary>
+        /// Set list to empty list for each test.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.list = null;
+        }
+
+        /// <summary>
+        /// Test Random with valid sizes.
+        /// </summary>
+        /// <param name="size">Size of list to create.</param>
+        [TestCase(0)]
+        [TestCase(1)]
+        public void TestRandomValidSize(int size)
+        {
+            Assert.DoesNotThrow(() => RandomList.Random());
+        }
+
+        /// <summary>
+        /// Test Random throws exception with invalid size.
+        /// </summary>
+        /// <param name="size">Size of list to create.</param>
+        [TestCase(-1)]
+        public void TestRandomInvalidSize(int size)
+        {
+            Assert.Throws<ArgumentException>(() => RandomList.Random());
+        }
+
+        /// <summary>
+        /// Test correct number of elements are added to list.
+        /// </summary>
+        /// <param name="size">Size of list to create.</param>
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestRandomSize(int size)
+        {
+            this.list = RandomList.Random();
+            Assert.AreEqual(size, this.list.Count);
+        }
+    }
+}
